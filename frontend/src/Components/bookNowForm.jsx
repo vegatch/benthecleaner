c6946946204de710867ebdb139f4443f45b63da1
Stravinsky Anglade <stravinsky.anglade@mykenzie.snhu.edu>
2023-01-06 04:53:06 -0500

    adding state to form booknow page

--- frontend/src/Components/bookNowForm.jsx
+++ frontend/src/Components/bookNowForm.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
 import "../CSS/book.css";
 
 export function BookNowForm() {
+    // ------------------
+    const [count, setCount] = useState(0)  
+    const handleIncrement = (e) => {
+        e.preventDefault();
+        setCount(count + 1)
+    }
+    const handleDecrement = (e) => {
+        e.preventDefault();
+        count > 0 ? setCount(count - 1) : setCount(0);
+    }
+    const countHandler = e => {
+        e.preventDefault();
+        setCount(e.target.value);
+
+    };
+
+    // ------------------
+    // ------------------- Form states
+    // const [bookInfo, setBookInfo] = useState({
+    //     bedroom: 0,
+    //     bathroom: 0,
+    // });
+
+    // const bookIncrement = (e) => {
+    //     e.preventDefault();
+    //     setBookInfo({countHandler})
+    // }
+    // const bookDecrement = (e) => {
+    //     e.preventDefault();
+    //     setBookInfo({handleDecrement});
+    // }
+    // const bookHandler = e => {
+    //     e.preventDefault();
+    //     setCount(e.target.value);
+
+    // };
+
+    // --------------------
     
     return(
         <div className="form-container">
@@ -13,10 +52,29 @@ export function BookNowForm() {
                 <form>
                     <fieldset>
                         <legend>Space to be cleaned</legend>
-                        <label>
-                            # of bedrooms
-                            <input name="name"  />                            
+                        <label className="label-flex-container">
+                            # of bedrooms:
+                            <input type="text"
+                                name="bedroom"
+                                onChange={countHandler}
+                                value={count} />  
+                            <div className="btn">
+                                <button onClick={handleIncrement}>^</button>
+                                <button className="btn2" onClick={handleDecrement} disabled={count === 0 ? true : false}>^</button>
+                            </div>                          
                         </label>
+                        <label className="label-flex-container">
+                            # of bathrooms:
+                            <input type="text"
+                                name="bathroom"
+                                onChange={countHandler}
+                                value={count} />  
+                            <div className="btn">
+                                <button onClick={handleIncrement}>^</button>
+                                <button className="btn2" onClick={handleDecrement} disabled={count === 0 ? true : false}>^</button>
+                            </div>                          
+                        </label>
+                        
                     </fieldset>
                     {/* <fieldset></fieldset>
                     <fieldset></fieldset>
