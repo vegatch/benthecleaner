ae90067e4cf14a71f7a2433d6d4b0ece20a7d77b
Stravinsky Anglade <stravinsky.anglade@mykenzie.snhu.edu>
2023-03-26 08:17:17 -0400

    Updates

---
+++ precache-manifest.cdc85fb60d3c194c29f597b82eaf9d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9890e53c199da3f0b58",
+    "url": "vegatch.github.io/benthecleaner/static/css/main.36817e91.chunk.css"
+  },
+  {
+    "revision": "d9890e53c199da3f0b58",
+    "url": "vegatch.github.io/benthecleaner/static/js/main.9a122b76.chunk.js"
+  },
+  {
+    "revision": "ea5b9f7c85fa4cd9168a",
+    "url": "vegatch.github.io/benthecleaner/static/js/runtime~main.f287c870.js"
+  },
+  {
+    "revision": "32caedf68ca51ed6f0b8",
+    "url": "vegatch.github.io/benthecleaner/static/js/2.ad76f560.chunk.js"
+  },
+  {
+    "revision": "5da9ff51f7d1876abd29e0b61a978961",
+    "url": "vegatch.github.io/benthecleaner/static/media/logo.5da9ff51.png"
+  },
+  {
+    "revision": "9a861f483a854b7afe7a37b01ec91815",
+    "url": "vegatch.github.io/benthecleaner/static/media/house-cleaning.9a861f48.jpg"
+  },
+  {
+    "revision": "e9eb59c753d62c657e494607416bed0a",
+    "url": "vegatch.github.io/benthecleaner/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "vegatch.github.io/benthecleaner/precache-manifest.cdc85fb60d3c194c29f597b82eaf9d1a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("vegatch.github.io/benthecleaner/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
